a5f0252c67c0eb7ac64781605bbc3217108a9842
linster <linster247@gmail.com>
2015-08-13 23:42:21 -0400

    Fixed title tag on landing page

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('static'));
+app.use('/', express.static('static'));
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
