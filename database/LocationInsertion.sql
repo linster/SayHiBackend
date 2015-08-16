6a8f1a31d62394f2c36bbc8fbf15e5dee5518d1f
linster <linster247@gmail.com>
2015-08-15 22:09:01 -0600

    Update db testing

---
+++ database/LocationInsertion.sql
@@ -0,0 +1,6 @@
+﻿--Get points from http://www.gps-coordinates.net/
+
+insert into location."LoggedLocations"("UserId", "AccuracyRadius", "Point", "DateLogged") 
+VALUES (1, 12, ST_GeomFromText('POINT(-113.593883 53.522043)', 4326), now());
+
+select * from location."LoggedLocations";
