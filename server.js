f60584bb94f5983a5572e97db4906c689caaec76
linster <linster247@gmail.com>
2015-08-19 12:50:01 -0400

    Switched to massive.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var db_config = require('./config.js');
 var express = require('express');
 var app = express();
-
-var http = require('http');
 var massive = require("massive");
 
+/* Connect to postgres */
+var connectionString = "postgres://" + db_config.user +":"+ db_config.password +"@"+ db_config.host +"/"+ db_config.database
+console.log(connectionString);
+var massiveInstance = massive.connectSync({connectionString: connectionString});
+
+
+/* Set up global db reference */
+app.set('db', massiveInstance);
 
 app.use('/', express.static('static'));
 app.get('/api', function (req, res) {
