eb506c96e79749382f56789bb7aa6c6f1a3d5f4f
linster <linster247@gmail.com>
2015-08-23 16:58:37 -0400

    Local auth login is working

--- auth.js
+++ auth.js
@@ -12,6 +12,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
 var FacebookStrategy = require('passport-facebook').Strategy
 
+/* Set up strategies */
 passport.use(new LocalStrategy({ passReqToCallback: true, session: false},
 	function(req, username, password, done) {
 		req.db.profile.Users.findOne({username : username}, function(err, user){
@@ -19,65 +20,63 @@ passport.use(new LocalStrategy({ passReqToCallback: true, session: false},
 			if(!user) {
 				return done(null, false, {message: 'User not found'});
 			}
+			/* Compare passwords with bcrypt               */
+			/* https://github.com/ncb000gt/node.bcrypt.js/ */
 			bcrypt.compare(password, user.password, function(err, res) {
 				if (res == false){
 					return done(null, false, 
 					     {message: 'Incorrect password.'});
 				}
 			});
+			//TODO: Might want to move this inside the callback above...
 			return done(null, user);
 		});
-		//stuff to lookup user
-		//http://passportjs.org/docs
-		//https://github.com/ncb000gt/node.bcrypt.js/
 	}
 ));
 
-//app.use(flash());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(passport.initialize());
+/*  Authenticated session persistence */
+/*  https://github.com/passport/express-4.x-local-example/blob/master/server.js */
 
-
-app.get('/login', function(req, res) {
-	//console.log(flash('error'));
-	res.sendFile(path.join(__dirname, '/static/login.html') )
+passport.serializeUser(function(user, cb) {
+  cb(null, user.Id);
 });
 
+passport.deserializeUser(function(id, req, cb) {
+  req.db.users.findById(id, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+
+/* Set up Express for passport sessions */
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('cookie-parser')());
+app.use(require('express-session')({ secret: 'SayWUT, Crazy Boris?', 
+				     resave: false, 
+				     saveUninitialized: false }));
+
+/* Initialize passport */
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+/* Serve up static login page */
+app.get('/login', function(req, res) {
+	res.sendFile(path.join(__dirname, '/static/login.html') )
+});
+/* Handle form postback */
 app.post('/login', passport.authenticate('local'), 
 	function(req, res){
 		res.redirect('/login/' + req.user.username);
 	}
 );
-
-
+/* Logout Route */
 app.get('/logout', function(req, res) {
 	req.logout();
 	res.redirect('/');
 });
 
 
-
-
-/*
-// Treat this as a constructor.
-module.exports = function(papp) {
-
-	//Copy app into global for file
-	app = papp;
-	db = papp.settings.db
-	initializeauth();	
-
-	//return { 
-	//	 pass: passport
-	//}
-	//Return passport
-	//Return references to all the vars...
-
-};
-
-
-
-
-*/
 
 
